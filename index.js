d23b2e6bd0d47ef78a43ce579882f307f71d3fb8
Hyper0x0 <122046226+Hyper0x0@users.noreply.github.com>
2025-05-11 21:23:53 +0200

    Organise entry point for export

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+//Telegram Bot
 export * from './telegram-bot/notifier/src/tg-bot-client.js';
+//Web3
 export * from './web3/token/ERC20/src/erc20.js';
+//Utils
 export * from './utils/src/response.utils.js';
 export * from './utils/src/tx.utils.js';
 export * from './utils/src/gas.utils.js';
+export * from './utils/src/http.utils.js';
