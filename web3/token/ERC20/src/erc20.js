b935997f4f43bd565b05de257a3734e77975ca30
Hyper0x0 <122046226+Hyper0x0@users.noreply.github.com>
2025-05-13 23:06:01 +0200

    Clean code optimizations for erc20

--- web3/token/ERC20/src/erc20.js
+++ web3/token/ERC20/src/erc20.js
@@ -175,19 +175,17 @@ export class ERC20 {
      */
     async bkApprove(_token, _amount, _spender, _gasPriceIncreasePercent = 0, _gasPrice = undefined) { 
         try {
-            let gasPrice;
-            if (_gasPrice) {
-                gasPrice = ethers.parseUnits(_gasPrice, 'gwei');
-            } else {
-                gasPrice = await calculateGasPrice(this.rpcProvider, _gasPriceIncreasePercent, this.EIP1559);
-                if (!gasPrice.success) {
-                    return createResponse(
-                        false,
-                        gasPrice.message,
-                        gasPrice.data,
-                        `ERC20.bkApprove -- ${gasPrice.source}`
-                    );
-                }
+            const gasPrice = _gasPrice 
+                ? ethers.parseUnits(_gasPrice, 'gwei') 
+                : await calculateGasPrice(this.rpcProvider, _gasPriceIncreasePercent, this.EIP1559);
+
+            if (!gasPrice.success && !_gasPrice) {
+                return createResponse(
+                    false,
+                    gasPrice.message,
+                    gasPrice.data,
+                    `kyberswap.swap -- ${gasPrice.source}`
+                );
             }
 
             const contract = new ethers.Contract(_token, this.erc20Abi, this.signer);
@@ -201,8 +199,8 @@ export class ERC20 {
                 );
             }
 
-            const txGasOptions = getTxGasOptions(this.EIP1559, gasLimit, gasPrice);
-            const tx = await contract.approve(_spender, _amount, txGasOptions);
+            const txGasParams = getTxGasOptions(this.EIP1559, gasLimit, gasPrice);
+            const tx = await contract.approve(_spender, _amount, txGasParams);
             await tx.wait(this.numberConfirmation);
 
             return createResponse(true, 'success', { hash: tx.hash }, 'ERC20.bkApprove');
@@ -229,19 +227,17 @@ export class ERC20 {
      */
     async bkTransfer(_token, _to, _amount, _gasPriceIncreasePercent = 0, _gasPrice = undefined) { 
         try {
-            let gasPrice;
-            if (_gasPrice) {
-                gasPrice = ethers.parseUnits(_gasPrice, 'gwei');
-            } else {
-                gasPrice = await calculateGasPrice(this.rpcProvider, _gasPriceIncreasePercent, this.EIP1559);
-                if (!gasPrice.success) {
-                    return createResponse(
-                        false,
-                        gasPrice.message,
-                        gasPrice.data,
-                        `ERC20.bkTransfer -- ${gasPrice.source}`
-                    );
-                }
+            const gasPrice = _gasPrice 
+                ? ethers.parseUnits(_gasPrice, 'gwei') 
+                : await calculateGasPrice(this.rpcProvider, _gasPriceIncreasePercent, this.EIP1559);
+
+            if (!gasPrice.success && !_gasPrice) {
+                return createResponse(
+                    false,
+                    gasPrice.message,
+                    gasPrice.data,
+                    `kyberswap.swap -- ${gasPrice.source}`
+                );
             }
     
             const contract = new ethers.Contract(_token, this.erc20Abi, this.signer);
@@ -255,8 +251,8 @@ export class ERC20 {
                 );
             }
     
-            const txGasOptions = getTxGasOptions(this.EIP1559, gasLimit, gasPrice);
-            const tx = await contract.transfer(_to, _amount, txGasOptions);
+            const txGasParams = getTxGasOptions(this.EIP1559, gasLimit, gasPrice);
+            const tx = await contract.transfer(_to, _amount, txGasParams);
             await tx.wait(this.numberConfirmation);
     
             return createResponse(true, 'success', { hash: tx.hash }, 'ERC20.bkTransfer');
