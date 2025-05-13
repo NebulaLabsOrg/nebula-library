9c9c8f4f953a95d8fff445fb5e09484bf1310096
Hyper0x0 <122046226+Hyper0x0@users.noreply.github.com>
2025-05-13 23:08:02 +0200

    Exports new libraries

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 export * from './telegram-bot/notifier/src/tg-bot-client.js';
 //Web3
 export * from './web3/token/ERC20/src/erc20.js';
+export * from './web3/dex/aggregator/kyberswap/src/kyberswap.js';
 //Utils
 export * from './utils/src/response.utils.js';
 export * from './utils/src/tx.utils.js';
 export * from './utils/src/gas.utils.js';
 export * from './utils/src/http.utils.js';
+export * from './utils/src/ethers.utils.js';
