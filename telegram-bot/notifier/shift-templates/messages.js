c10b5feb5a0c7d16b8c86cf5b2628b85f3a08f6d
Gloom0x0 <gloomneon@gmail.com>
2025-06-19 09:18:23 +0200

    Updated tg templates

--- telegram-bot/notifier/shift-templates/messages.js
+++ telegram-bot/notifier/shift-templates/messages.js
@@ -3,6 +3,7 @@
  * @param {string} _version - The version of the vault.
  * @param {string} _decision - The current state or decision.
  * @param {number} _tvl - Total Value Locked in USD.
+ * @param {number} _percLoss - Percentage loss of the vault.
  * @param {number} _apyOverall - Overall Annual Percentage Yield (APY).
  * @param {string} _market - The market strategy name.
  * @param {number} _percOnPerp - Percentage allocated to the market strategy.
@@ -12,14 +13,17 @@
  * @param {string} _provider2 - Name of the second provider.
  * @param {string} _legProvider1 - Details for the first provider's leg.
  * @param {string} _legProvider2 - Details for the second provider's leg.
+ * @param {number} _percLossPerp1 - Percentage loss for the first provider.
+ * @param {number} _percLossPerp2 - Percentage loss for the second provider.
  * @returns {string} - Formatted Telegram message.
  */
-export function shiftStrategy(_version, _decision, _tvl, _apyOverall, _market, _percOnPerp, _apyMarket, _apysUSDe, _provider1, _provider2, _legProvider1, _legProvider2) {
+export function shiftStrategy(_version, _decision, _tvl, _percLoss, _apyOverall, _market, _percOnPerp, _apyMarket, _apysUSDe, _provider1, _provider2, _legProvider1, _legProvider2, _percLossPerp1, _percLossPerp2) {
     return `
             🏦 <b>SHIFTing Vault v${_version}</b> 🏦
             ⚙️ <b>State:</b> ${_decision} ⚙️
 
             💵 <b>TVL:</b> $<code>${_tvl}</code>
+            📉 <b>Loss:</b><code>${_percLoss}%</code>
             📈 <b>APY (current):</b> <code>${_apyOverall.toFixed(2)}%</code>
             🔹 <b>Strategy:</b> <code>${_market} (${_percOnPerp}%)</code> & <code>sUSDe (${100 - _percOnPerp}%)</code>
 
@@ -27,8 +31,8 @@ export function shiftStrategy(_version, _decision, _tvl, _apyOverall, _market, _
             📈 <b>APY ${_market}:</b> <code>${_apyMarket}%</code>
             📈 <b>APY sUSDe:</b> <code>${_apysUSDe}%</code>
 
-            ➡️ <b>${_provider1}</b> (<code>${_legProvider1}</code>)
-            ➡️ <b>${_provider2}</b> (<code>${_legProvider2}</code>)
+            ➡️ <b>${_provider1}</b> (<code>${_legProvider1}</code>) - <b>Loss:</b><code>${_percLossPerp1}%</code>
+            ➡️ <b>${_provider2}</b> (<code>${_legProvider2}</code>) - <b>Loss:</b><code>${_percLossPerp2}%</code>
 
             `;
 }
