dc1df80bdec85a51c0d20ba1898f544801b6d14b
Gloom0x0 <gloomneon@gmail.com>
2025-07-14 13:16:02 +0200

    Added base structure for EdgeX perp

---
+++ web3/dex/perp/edgex/src/edgex.js
@@ -0,0 +1,11 @@
+import { createInstance } from '../../../../../utils/src/http.utils.js';
+
+import { edgexEnum } from './enum.js';
+
+export { edgexEnum };
+
+export class Edgex {
+    constructor(parameters) {
+        
+    }
+}
